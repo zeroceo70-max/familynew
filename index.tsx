48e832fde59724b29fd0d71f25e862afd975a037
zeroceo70-max <zeroceo70@gmail.com>
2025-11-20 17:24:43 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App'
 import './index.css'  // if you have this file, otherwise delete this line
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
