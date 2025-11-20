154339d93f34ac925786c1cc311b83907d576e6a
zeroceo70-max <zeroceo70@gmail.com>
2025-11-20 08:24:16 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
-import "./index.css";
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-//import './index.css'  // if you have this file, otherwise delete this line
+import './index.css'  // if you have this file, otherwise delete this line
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
