d2f6057cf5fd3b8e5125dcde03702af37ebae101
zeroceo70-max <zeroceo70@gmail.com>
2025-11-20 08:07:51 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,13 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx",
+    "./**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      colors: {
-        'brand-blue': '#3B82F6',
-        'brand-green': '#10B981',
-        'soft-blue': '#EFF6FF',
-        'soft-green': '#ECFDF5',
-      }
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
